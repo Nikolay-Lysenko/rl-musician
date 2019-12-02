9828428a9513d2e8ccdbb9e08480691ad4b90fbb
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2019-12-02 22:48:55 +0300

    feat: Find positions of allowed pitches

--- rlmusician/environment/piece.py
+++ rlmusician/environment/piece.py
@@ -8,7 +8,51 @@ from typing import Any, Dict, List
 from sinethesizer.io.utils import get_note_to_position_mapping
 
 
+N_SEMITONES_PER_OCTAVE = 12
 NOTE_TO_POSITION = get_note_to_position_mapping()
+
+
+def get_positions_from_scale(tonic: str, scale: str) -> List[int]:
+    """"""
+    scale_patterns = {
+        'major': [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
+        'minor': [1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0]
+    }
+    tonic_position = NOTE_TO_POSITION[tonic + '1']
+    positions_from_scale = [
+        x for x in range(len(NOTE_TO_POSITION))
+        if scale_patterns[scale][(x - tonic_position) % N_SEMITONES_PER_OCTAVE]
+    ]
+    return positions_from_scale
+
+
+def get_tonic_triad_positions(tonic: str, scale: str) -> List[int]:
+    """"""
+    scale_patterns = {
+        'major': [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+        'minor': [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0]
+    }
+    tonic_position = NOTE_TO_POSITION[tonic + '1']
+    positions_from_scale = [
+        x for x in range(len(NOTE_TO_POSITION))
+        if scale_patterns[scale][(x - tonic_position) % N_SEMITONES_PER_OCTAVE]
+    ]
+    return positions_from_scale
+
+
+def slice_positions(
+        positions: List[int],
+        lowest_note: str,
+        highest_note: str,
+) -> List[int]:
+    """"""
+    lowest_position = NOTE_TO_POSITION[lowest_note]
+    highest_position = NOTE_TO_POSITION[highest_note]
+    sliced_positions = [
+        x for x in positions
+        if lowest_position <= x <= highest_position
+    ]
+    return sliced_positions
 
 
 class Piece:
@@ -19,7 +63,7 @@ class Piece:
             tonic: str,
             scale: str,
             n_measures: int,
-            line_params: Dict[str, Any],
+            line_params: List[Dict[str, Any]],
             rendering_params: Dict[str, Any]
     ):
         """"""
@@ -29,18 +73,28 @@ class Piece:
         self.line_params = line_params
         self.rendering_params = rendering_params
 
-        self.__validate_line_params()
+        self.positions_from_scale = get_positions_from_scale(tonic, scale)
+        self.tonic_triad_positions = get_tonic_triad_positions(tonic, scale)
 
-        self.lines = None
+        self.lines = []
+        self.allowed_positions = []
+        for params in line_params:
+            self.__create_line(params)
         self.piano_roll = None
-        self.__create_lines()
         self.__create_piano_roll()
 
-    def __validate_line_params(self) -> None:
+    def __create_line(self, params: Dict[str, Any]) -> None:
         """"""
-
-    def __create_lines(self) -> None:
-        """"""
+        sliced_positions = slice_positions(
+            self.positions_from_scale,
+            params['lowest_note'], params['highest_note']
+        )
+        if not sliced_positions:
+            raise ValueError(
+                f"No pitches from {self.tonic}-{self.scale} are between "
+                f"{params['lowest_note']} and {params['highest_note']}."
+            )
+        self.allowed_positions.append(sliced_positions)
 
     def __create_piano_roll(self) -> None:
         """"""
