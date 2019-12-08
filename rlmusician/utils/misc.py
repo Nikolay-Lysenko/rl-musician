8e706eccd546a7a6e4f61e5ca7c8d9f3a4966079
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2019-12-08 12:10:20 +0300

    refac: Delete code that is not needed any more

--- rlmusician/utils/misc.py
+++ rlmusician/utils/misc.py
@@ -8,8 +8,6 @@ Author: Nikolay Lysenko
 import multiprocessing as mp
 from typing import Any, Callable, List, Optional, Tuple
 
-import numpy as np
-
 
 def map_in_parallel(
         fn: Callable,
@@ -43,86 +41,3 @@ def map_in_parallel(
         pool.close()
         pool.join()
     return results
-
-
-def shift_horizontally(arr: np.ndarray, shift: int) -> np.ndarray:
-    """
-    Shift 2D array along horizontal axis.
-
-    Number of columns of output array is the same as that of input array,
-    because non-fitting values are removed and gaps are padded with zeros.
-
-    :param arr:
-        2D array
-    :param shift:
-        signed value of shift, positive for shift to the right
-        and negative for shift to the left
-    :return:
-        shifted array
-    """
-    if shift == 0:
-        return arr
-    result = np.zeros_like(arr)
-    if shift > 0:
-        result[:, shift:] = arr[:, :-shift]
-    else:
-        result[:, :shift] = arr[:, -shift:]
-    return result
-
-
-def shift_vertically(arr: np.ndarray, shift: int) -> np.ndarray:
-    """
-    Shift 2D array along vertical axis.
-
-    Length of output array is the same as length of input array,
-    because non-fitting values are removed and gaps are padded with zeros.
-
-    :param arr:
-        2D array
-    :param shift:
-        signed value of shift, positive for upward shift
-        and negative for downward shift
-    :return:
-        shifted array
-    """
-    if shift == 0:
-        return arr
-    result = np.zeros_like(arr)
-    if shift > 0:
-        result[:-shift, :] = arr[shift:, :]
-    else:
-        result[-shift:, :] = arr[:shift, :]
-    return result
-
-
-def apply_rolling_aggregation(
-        arr: np.ndarray, max_lag: int, fn_name: str, lags_only: bool = True
-) -> np.ndarray:
-    """
-    Compute rolling statistics of array.
-
-    :param arr:
-        2D array with horizontal axis representing time
-    :param max_lag:
-        maximum lag to include
-    :param fn_name:
-        name of aggregation function, one of 'min', 'mean', and 'max'
-    :param lags_only:
-        if it is `True`, the current value is excluded from computation;
-        if it is `False`, rolling window includes the current value
-    :return:
-        array of rolling statistics
-    """
-    range_start = 1 if lags_only else 0
-    lagged_rolls = [
-        shift_horizontally(arr, i).reshape(arr.shape + (1,))
-        for i in range(range_start, max_lag + 1)
-    ]
-    lagged_roll = np.concatenate(lagged_rolls, axis=-1)
-    name_to_fn = {
-        'min': lambda x: x.min(axis=-1),
-        'mean': lambda x: x.mean(axis=-1),
-        'max': lambda x: x.max(axis=-1)
-    }
-    rolling_stats_roll = name_to_fn[fn_name](lagged_roll)
-    return rolling_stats_roll
