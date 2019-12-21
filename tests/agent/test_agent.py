2a0127b134853b6dc706368ccfc6e6259d2465c8
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2019-12-21 11:53:10 +0300

    ci: Test uncovered methods of CounterpointEnvAgent

--- tests/agent/test_agent.py
+++ tests/agent/test_agent.py
@@ -12,6 +12,7 @@ import pytest
 
 from rlmusician.agent.agent import CounterpointEnvAgent
 from rlmusician.agent.actor_model import create_actor_model
+from rlmusician.environment import CounterpointEnv, Piece
 
 
 class TestCounterpointEnvAgent:
@@ -58,14 +59,111 @@ class TestCounterpointEnvAgent:
             observation: np.ndarray, actions: List[int], expected: np.ndarray
     ) -> None:
         """Test `represent_actions` method."""
-        dummy_model = create_actor_model((10,), 5)
         agent = CounterpointEnvAgent(
-            lambda x, y: dummy_model,
+            create_actor_model,
             len(observation),
             n_lines,
             n_movements_per_line,
-            hidden_layer_size=0,
+            hidden_layer_size=3,
             softmax_temperature=1
         )
         result = agent.represent_actions(observation, actions)
         np.testing.assert_equal(result, expected)
+
+    @pytest.mark.parametrize(
+        "observation_len, n_lines, n_movements_per_line, hidden_layer_size, "
+        "flat_weights",
+        [
+            (
+                # `observation_len`
+                3,
+                # `n_lines`
+                2,
+                # `n_movements_per_line`
+                3,
+                # `hidden_layer_size`
+                5,
+                # `flat_weights`
+                np.array([1 for _ in range(56)])
+            ),
+        ]
+    )
+    def test_set_weights(
+            self, observation_len: int, n_lines: int,
+            n_movements_per_line: int, hidden_layer_size: int,
+            flat_weights: np.ndarray
+    ) -> None:
+        """Test `set_weights` method."""
+        agent = CounterpointEnvAgent(
+            create_actor_model,
+            observation_len,
+            n_lines,
+            n_movements_per_line,
+            hidden_layer_size,
+            softmax_temperature=1
+        )
+        agent.set_weights(flat_weights)
+        model_weights = agent.model.get_weights()
+        flat_model_weights = np.hstack((x.flatten() for x in model_weights))
+        np.testing.assert_equal(flat_model_weights, flat_weights)
+
+    @pytest.mark.parametrize(
+        "observation_len, n_lines, n_movements_per_line, hidden_layer_size, "
+        "env",
+        [
+            (
+                # `observation_len`
+                25,
+                # `n_lines`
+                2,
+                # `n_movements_per_line`
+                5,
+                # `hidden_layer_size`
+                5,
+                # `env`
+                CounterpointEnv(
+                    piece=Piece(
+                        tonic='C',
+                        scale='major',
+                        n_measures=5,
+                        max_skip=2,
+                        line_specifications=[
+                            {
+                                'lowest_note': 'G3',
+                                'highest_note': 'G4',
+                                'start_note': 'C4',
+                                'end_note': 'C4'
+                            },
+                            {
+                                'lowest_note': 'G4',
+                                'highest_note': 'G5',
+                                'start_note': 'G5',
+                                'end_note': 'C5'
+                            },
+                        ],
+                        rendering_params={}
+                    ),
+                    observation_decay=0.75,
+                    reward_for_dead_end=-100,
+                    scoring_coefs={'lines_correlation': 1},
+                    scoring_fn_params={},
+                ),
+            ),
+        ]
+    )
+    def test_run_episode(
+            self, observation_len: int, n_lines: int,
+            n_movements_per_line: int, hidden_layer_size: int,
+            env: CounterpointEnv
+    ) -> None:
+        """Test that `run_episode` method does not fail."""
+        agent = CounterpointEnvAgent(
+            create_actor_model,
+            observation_len,
+            n_lines,
+            n_movements_per_line,
+            hidden_layer_size,
+            softmax_temperature=1
+        )
+        reward = agent.run_episode(env)
+        assert isinstance(reward, float)
