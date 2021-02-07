ac1cba1a79993a9323b45a825afe3f2c933f5c4a
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2021-02-07 15:40:17 +0300

    chore: Update package version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(current_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='rl-musician',
-    version='0.4.4',
+    version='0.4.5',
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -44,7 +44,7 @@ setup(
         'numpy',
         'pretty-midi',
         'PyYAML',
-        'sinethesizer>=0.5.0',
+        'sinethesizer>=0.5.1',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
